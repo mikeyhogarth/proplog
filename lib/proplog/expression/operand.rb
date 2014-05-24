87ba5105f2299334cb498652cbcf618d97488cc8
Mikey Hogarth <mikehogarth20@hotmail.com>
2014-05-24 09:36:47 +0100

    Further encapsulating operand

--- lib/proplog/expression/operand.rb
+++ lib/proplog/expression/operand.rb
@@ -8,6 +8,23 @@ module Proplog
   module Expression
     module Operand
 
+      #
+      # operand
+      #
+      # Available as an instance method. Consults the internal hash
+      # to establish which operand should be used for this class.
+      #
+      def operand
+        OPERANDS[self.class.name]
+      end
+      
+      private
+
+      #
+      # In the unlikely event that a new operand is introduced into
+      # propositional calculus, add it here along with a terminal or non-terminal
+      # class in the appropriate folder.
+      #
       OPERANDS = {
         "Proplog::Expression::Conjunction" => "∧",
         "Proplog::Expression::Disjunction" => "∨",
@@ -15,9 +32,6 @@ module Proplog
         "Proplog::Expression::Negation" => "¬"
       }
 
-      def operand
-        OPERANDS[self.class.name]
-      end
     end
   end
 end
